a5c9bda5a3ca0ed811778a81f1e97b79775a9211
Vitor Moutim <moutimg@gmail.com>
2024-08-03 18:18:00 -0300

    feat: configura cors

--- back/atm/Program.cs
+++ back/atm/Program.cs
@@ -91,6 +91,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 app.UseAuthentication();
 app.UseAuthorization();
 
